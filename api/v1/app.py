88019818f0b07d99ba10a958cd5c5f1d76cb4ad1
Ivan Mickisz <ivan.mickisz3@gmail.com>
2022-05-20 11:21:19 +0200

    added method to handle error 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
-"""comment"""
+"""This module starts a Flask web application"""
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-    
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def tearitdown(exception):
+    """This method remove the current SQLAlchemy Session"""
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(exception):
+    """This method handle error 404"""
+    return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == '__main__':
     if getenv('HBNB_API_HOST') and getenv('HBNB_API_PORT') is None:
