e353e6432fe48581f5610a74136571f6c79a4ec4
Anthony-6 <anthony.lacoste5@gmail.com>
2022-05-20 12:39:37 +0200

    udpate pycodestyle

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+"""index file, where everything start and end"""
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
@@ -9,13 +10,18 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
+
 @app_views.route('/status')
 def status():
+    """Return that the status is ok in json format"""
     stat = {"status": "OK"}
     return jsonify(stat)
 
+
 @app_views.route('/stats')
 def countstorage():
+    """count all element inside each class and return
+       class and number of objet inside in json format"""
     classes = {"amenity": storage.count(Amenity),
                "city": storage.count(City),
                "place": storage.count(Place),
