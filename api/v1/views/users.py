4f818f1ba96e5a0d77fdc001489394eadf4e9901
Ivan Mickisz <ivan.mickisz3@gmail.com>
2022-05-23 17:24:06 +0200

    corrected user.py (task 10)

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,47 +1,59 @@
 #!/usr/bin/python3
-"""Create a new view for State objects that
-handles all default RESTFul API actions"""
+"""Create a new view for State objects that handles all default RESTFul API
+actions"""
+
 from api.v1.views import app_views
 from flask import request, abort, jsonify
 from models.user import User
 from models import storage
 
 
-@app_views.route('/users/<user_id>', methods=['GET', 'DELETE', 'PUT'])
+@app_views.route('/users/<user_id>', methods=['GET', 'DELETE', 'PUT'],
+                 strict_slashes=False)
 def usersWithId(user_id=None):
     """Methods that retrieves all methods for users with id"""
     userId = storage.get(User, user_id)
     if userId is None:
-        abort(404, 'Not found')
+        return abort(404)
 
     if request.method == 'GET':
+        """Retrieves a user of a given user_id"""
         return jsonify(userId.to_dict())
 
     if request.method == 'DELETE':
+        """Deletes a user of a given user_id """
         userId.delete()
-        del userId
-        return jsonify({}), 200
+        storage.save()
+        return jsonify({})
 
     if request.method == 'PUT':
+        """Update an user of a given user_id"""
         if request.get_json() is None:
-            abort(400, 'Not a JSON')
+            return abort(400, 'Not a JSON')
+        toIgnore = ["id", "created_at", "updated_it"]
+        for key, value in request.get_json().items():
+            if value not in toIgnore:
+                setattr(userId, key, value)
         userId.save()
-        return jsonify(userId.to_dict())
+        return jsonify(userId.to_dict()), 200
 
 
-@app_views.route('/users/', methods=['POST', 'GET'])
+@app_views.route('/users', methods=['POST', 'GET'],
+                 strict_slashes=False)
 def usersNoId():
     """Methods that retrieves all methods for users without id"""
     if request.method == 'POST':
+        """Create a new user"""
         if request.get_json() is None:
             abort(400, 'Not a JSON')
         if request.get_json().get('name') is None:
             abort(400, 'Missing name')
         newUser = User(**request.get_json())
         newUser.save()
-        return jsonify(newUser.to_dict()), 200
+        return jsonify(newUser.to_dict()), 201
 
     if request.method == 'GET':
+        """Retrieves get method for all users"""
         allUser = storage.all(User)
-        allUser = [allObject.to_dict() for allObject in allUser.values()]
-        return jsonify(allUser)
+        user = list(allObject.to_dict() for allObject in allUser.values())
+        return jsonify(user)
