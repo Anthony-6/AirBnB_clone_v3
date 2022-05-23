dffa74579848a63fdebeace346f0fda11d74f3d9
Ivan Mickisz <ivan.mickisz3@gmail.com>
2022-05-23 15:36:19 +0200

    try for task 8

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Create a new view for State objects that
-handles all default RESTFul API actions"""
+"""Create a new view for State objects that handles all default RESTFul
+API actions"""
+
 from api.v1.views import app_views
 from flask import request, abort, jsonify
 from models.state import State
@@ -10,17 +11,19 @@ from models import storage
 @app_views.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'])
 def statesWithId(state_id=None):
     """Methods that retrieves all methods for states with id"""
-    stateId = storage.get(State, state_id)
+    if state_id is None:
+        return abort(404)
+    stateId = storage.get("State", state_id)
+
     if request.method == 'GET':
         if stateId is None:
             return abort(404)
-        return jsonify(stateId.to_dict())
+        return jsonify(stateId.to_json())
 
     if request.method == 'DELETE':
         if stateId is None:
             return abort(404)
-        stateId.delete()
-        del stateId
+        storage.delete(sategeId)
         return jsonify({}), 200
 
     if request.method == 'PUT':
@@ -33,10 +36,10 @@ def statesWithId(state_id=None):
         if request.get_json() is None:
             return abort(400, 'Not a JSON')
         stateId.save()
-        return jsonify(stateId.to_dict()), 201
+        return jsonify(stateId.to_json()), 201
 
 
-@app_views.route('/states/', methods=['POST', 'GET'])
+@app_views.route('/states', methods=['POST', 'GET'])
 def statesNoId():
     """Methods that retrieves all methods for states without id"""
     if request.method == 'POST':
@@ -46,9 +49,9 @@ def statesNoId():
             return abort(400, 'Missing name')
         newState = State(**request.get_json())
         newState.save()
-        return jsonify(newState.to_dict()), 201
+        return jsonify(newState.to_json()), 201
 
     if request.method == 'GET':
-        allState = storage.all(State)
-        allState = [allObject.to_dict() for allObject in allState.values()]
+        Stock = storage.all("State")
+        allState = [allObject.to_json() for allObject in Stock.values()]
         return jsonify(allState)
