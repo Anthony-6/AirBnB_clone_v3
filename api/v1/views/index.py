5788ceb74e76d08e41458bda9d4d89a09af40ad4
Anthony-6 <anthony.lacoste5@gmail.com>
2022-05-23 12:49:24 +0200

    update index file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -23,11 +23,11 @@ def countstorage():
     """count all element inside each class and return
        class and number of objet inside in json format"""
     classes = {
-        "amenity": storage.count(Amenity),
-        "city": storage.count(City),
-        "place": storage.count(Place),
-        "review": storage.count(Review),
-        "state": storage.count(State),
-        "user": storage.count(User)
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
     }
     return classes
