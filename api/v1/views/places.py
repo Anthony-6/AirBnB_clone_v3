70a4694c45266598b8cb109e6ba0607d0853e53b
Ivan Mickisz <ivan.mickisz3@gmail.com>
2022-05-23 20:58:35 +0200

    try resolving bug

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Create a new view for State objects that
-handles all default RESTFul API actions"""
+"""Create a new view for State objects that handles all default RESTFul API
+actions"""
+
 from api.v1.views import app_views
 from flask import request, abort, jsonify
 from models.place import Place
@@ -12,28 +13,35 @@ from models import storage
                  strict_slashes=False)
 def placeWithId(city_id=None):
     """Methods that retrieves all methods for states with id"""
-    cityId = storage.get(City, city_id)
-    transformers = request.get_json()
     if request.method == 'GET':
-        if cityId is None:
-            return abort(404)
+        """Retrieves get method for all place"""
         for city in storage.all(City).values():
             if city.id == city_id:
-                list_place = [place.to_dict() for place in city.places]
-        return jsonify(list_place)
+                places_list = []
+                for place in city.places:
+                    place_list.append(place.to_dict())
+                return jsonify(place_list)
+        return abort(404)
 
     if request.method == 'POST':
+        """Create a new place"""
+        cityId = storage.get(City, city_id)
         if cityId is None:
             return abort(404)
-        if transformers is None:
-            return abort(400, 'Not a JSON')
-        if transformers.get('user_id'):
-            return abort(400, 'Missing user_id')
-        if transformers.get('name'):
-            return abort(400, 'Missing name')
-        newCity = City(**transformers)
-        newCity.save()
-        return jsonify(newCity.to_dict()), 201
+        r = request.get_json()
+        if r is None:
+            abort(400, 'Not a JSON')
+        if r.get('user_id') is None:
+            abort(400, "Missing user_id")
+        user = storage.get(User, r['user_id'])
+        if user is None:
+            return abort(404)
+        if r.get("name") is None:
+            abort(400, "Missing name")
+        r['city_id'] = city_id
+        new = Place(**r)
+        new.save()
+        return jsonify(new.to_dict()), 201
 
 
 @app_views.route('/places/<places_id>', methods=['GET', 'DELETE', 'PUT'],
@@ -41,28 +49,27 @@ def placeWithId(city_id=None):
 def placesId(places_id):
     """Methods that retrieves all methods for place without id"""
     placeId = storage.get(Place, places_id)
-    transformers = request.get_json()
-    if request.method == 'DELETE':
-        if Place is None:
-            return abort(404)
-        placeId.delete()
-        storage.save()
-        return jsonify({}), 200
-
-    if request.method == 'PUT':
-        if placeId is None:
-            return abort(404)
-        if transformers is None:
-            return abort(400, 'Not a JSON')
-        toIgnore = ['id', 'user_id', 'city_id', 'created_at', 'updated_at']
-        for key, value in transformers.items():
-            if value not in toIgnore:
-                setattr(placeId, key, value)
-        placeId.save()
-        return jsonify(placeId.to_dict()), 200
+    if placeId is None:
+        return abort(404)
 
     if request.method == 'GET':
-        if placeId is None:
-            return abort(404)
-        allPlace = storage.all(Place)
-        return jsonify(allPlace.to_dict())
+        """Retrieves a place of a given place_id"""
+        return jsonify(placeId.to_dict())
+
+    if request.method == 'DELETE':
+        """Deletes a place of a given place_id"""
+        placeId.delete()
+        storage.save()
+        return jsonify({})
+
+    if request.method == 'PUT':
+        """Update an place of a given place_id"""
+        r = request.get_json()
+        if r is None:
+            return abort(400, 'Not a JSON')
+        toIgnore = ['id', 'user_id', 'city_id', 'created_at', 'updated_at']
+        for key, value in r.items():
+            if key not in toIgnore:
+                setattr(placeId, key, value)
+        placeId.save()
+        return jsonify(placeId.to_dict())
